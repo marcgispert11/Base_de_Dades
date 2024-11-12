efe690850c659d29db5619996d9129fd4ada00ae
Marc <marc.gispert@enti.cat>
2024-11-12 11:26:06 +0100

    Exercici 3 del Segon Power Point Fet i Acabat

--- exercici_3.sql
+++ exercici_3.sql
@@ -1,35 +1,19 @@
-
-CREATE TABLE professors (
-	id INT UNSIGNED,
-	nif CHAR(9),
-	nom VARCHAR(20),
-	especialitat VARCHAR(15),
-	telefon VARCHAR(9),
-	PRIMARY KEY (id),
-	UNIQUE (nif)
+CREATE TABLE mecanics (
+    dni VARCHAR(9) PRIMARY KEY,
+    nom VARCHAR(20),
+    cognom VARCHAR(20),
+    edat INT
 );
 
-CREATE TABLE alumnes (
-	num_matricula INT UNSIGNED,
-	nom VARCHAR(20),
-	data_naixement DATE,
-	telefon INT UNSIGNED,
-	PRIMARY KEY (num_matricula)
-);
 
-CREATE TABLE assignatures (
-	codi_assignatura INT UNSIGNED,
-	nom VARCHAR(20),
-	id_professor INT UNSIGNED,
-	PRIMARY KEY (codi_assignatura),
-	FOREIGN KEY (id_professor) REFERENCES professors (id)
-);
 
-CREATE TABLE matriculacio (
-	num_matricula_alumne INT UNSIGNED,
-	codi_assignatura INT UNSIGNED,
-	any_academic INT UNSIGNED,
-	FOREIGN KEY (num_matricula_alumne) REFERENCES alumnes (num_matricula),
-	FOREIGN KEY (codi_assignatura) REFERENCES assignatures (codi_assignatura),
-	PRIMARY KEY (num_matricula_alumne)
-);
+CREATE TABLE vehicles (
+    matricula VARCHAR(7) PRIMARY KEY,
+    propietari VARCHAR(20),
+    marca VARCHAR(20),
+    dni VARCHAR(9),
+    FOREIGN KEY (dni) REFERENCES mecanics (dni)
+        ON DELETE SET NULL
+        ON UPDATE SET NULL
+
+); 
